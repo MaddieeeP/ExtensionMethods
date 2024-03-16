d11b01a5ecf8ba710dc6c22b0baab984c6252c4c
Maddie <maddieeeperryyy@gmail.com>
2024-03-16 12:45:10 +0000

    Removed Encoder

--- StringMethods.cs
+++ StringMethods.cs
@@ -5,43 +5,10 @@ using UnityEngine;
 
 public static class StringMethods
 {
-    public const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
     public static string Truncate(this string str, int maxLength)
     {
         if (string.IsNullOrEmpty(str)) return str;
 
         return str.Substring(0, Math.Min(str.Length, maxLength));
-    }
-
-    public static string PadShiftEncode(this string content, string pad, bool forwardShift = true)
-    {
-        if (string.IsNullOrEmpty(content) || string.IsNullOrEmpty(pad)) return "";
-
-        content = content.ToLower();
-        pad = pad.ToLower();
-
-        while (pad.Length < content.Length)
-        {
-            pad += pad;
-        }
-
-        string encodedString = "";
-        for (int i = 0; i < content.Length; i++) 
-        {
-            int charCode = forwardShift ? (int)content[i] + (int)pad[i] - 2 * (int)'z' : (int)content[i] - (int)pad[i];
-            charCode = charCode - 1;
-            while (charCode < 0)
-            {
-                charCode += 26;
-            }
-            while (charCode > 25)
-            {
-                charCode -= 26;
-            }
-            encodedString += letters[charCode].ToString().ToLower();
-        }
-
-        return encodedString;
     }
 }
