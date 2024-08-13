b91cf0c4d240cab5d9eabe145bd70043434925a3
Maddie <129887899+MaddieeeP@users.noreply.github.com>
2024-08-13 19:41:38 +0100

    Added string.RemoveFirstOccurence()

--- StringMethods.cs
+++ StringMethods.cs
@@ -11,4 +11,10 @@ public static class StringMethods
 
         return str.Substring(0, Math.Min(str.Length, maxLength));
     }
+
+    public static string RemoveFirstOccurence(this string str, string substring)
+    {
+        int index = str.IndexOf(substring);
+        return (index < 0) ? str : str.Remove(index, substring.Length);
+    }
 }
